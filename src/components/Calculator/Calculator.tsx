263c0e1d5bc49817854b8343b4520fc4f914ffa6
Pedro Rodrigues <contact@pedrohmr.com>
2023-03-29 20:26:59 -0300

    Fix margin in display to better show Not a number

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -64,7 +64,9 @@ const Calculator: React.FC = () => {
 
   return (
     <div className="bg-black w-80 px-3 py-10">
-      <div className="m-4 mt-24 text-5xl text-right break-all">{display}</div>
+      <div className="m-4 ml-0 mt-24 text-5xl text-right break-all">
+        {display}
+      </div>
       <div className="grid grid-rows-5 grid-flow-col gap-2">
         <div className="col-span-3">
           <div className="grid grid-cols-3 gap-2 place-content-center">
